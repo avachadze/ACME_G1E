06ffe12fdaf182c0cd3c65386a46df0346b1b66c
CarlosTaboada <ctaboadafe19dw31@ikzubirimanteo.com>
2020-10-28 12:45:45 +0100

    Create view for product

---
+++ resources/views/Product/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.layout')
+
+@section('style')
+
+    <link rel="stylesheet" media="screen and (max-width:399)" href="css/error.css">
+    <link rel="stylesheet" media="screen and (min-width:400px) and (max-width:599px)" href="css/all/main.css">
+    <link rel="stylesheet" media="screen and (min-width:600px) and (max-width:999px)" href="css//all/mainT.css">
+    <link rel="stylesheet" media="screen and (min-width:1000px)" href="css/all/mainC.css">
+@endsection
+
+@section('content')
+    <h1>Insert the data to create a new entry</h1>
+    <form enctype="multipart/form-data" action="{{ route('ProductController.store') }}" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name"><br>
+        <label for="shopID">Shop ID:</label>
+        <input type="text" name="shopID"><br>
+        <label for="quantity">Stock:</label>
+        <input type="text" name="quantity"><br>
+        <label for="picture">Product image:</label>
+        <input type="file" name="picture"><br>
+    </form>
+
+@endsection
